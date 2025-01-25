bae8838d62afcdbf37b657f9532d9c0281f84669
clwope <tahabolat2003@gmail.com>
2025-01-26 00:49:57 +0300

    added auction product post

--- src/components/admin/AddAuctionProduct.vue
+++ src/components/admin/AddAuctionProduct.vue
@@ -9,52 +9,51 @@
             <form class="row g-3">
                 <div class="col-md-6">
                     <label for="auction_product_brand" class="form-label">Marka</label>
-                    <input type="text" class="form-control" id="auction_product_brand">
+                    <input type="text" class="form-control" id="auction_product_brand" v-model="product.brand">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_model" class="form-label">Model</label>
-                    <input type="text" class="form-control" id="auction_product_model">
+                    <input type="text" class="form-control" id="auction_product_model" v-model="product.model">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_case_color" class="form-label">Kasa Rengi</label>
-                    <input type="email" class="form-control" id="auction_product_case_color">
+                    <input type="text" class="form-control" id="auction_product_case_color" v-model="product.caseColor">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_case_shape" class="form-label">Kasa Şekli</label>
-                    <input type="password" class="form-control" id="auction_product_case_shape">
+                    <input type="text" class="form-control" id="auction_product_case_shape" v-model="product.caseShape">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_cord_color" class="form-label">Kordon Rengi</label>
-                    <input type="email" class="form-control" id="auction_product_cord_color">
+                    <input type="text" class="form-control" id="auction_product_cord_color" v-model="product.bandColor">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_dial_color" class="form-label">Kadran Rengi</label>
-                    <input type="password" class="form-control" id="auction_product_dial_color">
+                    <input type="text" class="form-control" id="auction_product_dial_color" v-model="product.dialColor">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_gender" class="form-label">Cinsiyet</label>
-                    <select class="form-select" id="auction_product_gender">
+                    <select class="form-select" id="auction_product_gender" v-model="product.gender">
                         <option value="Unisex" selected>Unisex</option>
                         <option value="Erkek">Erkek</option>
                         <option value="Kadın">Kadın</option>
                     </select>
-                    <!-- <input type="email" class="form-control" id="auction_product_gender"> -->
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_technology" class="form-label">Teknoloji</label>
-                    <input type="password" class="form-control" id="auction_product_technology">
+                    <input type="text" class="form-control" id="auction_product_technology" v-model="product.technology">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_price" class="form-label">Fiyat</label>
-                    <input type="number" class="form-control" id="auction_product_price">
+                    <input type="number" min="0" class="form-control" id="auction_product_price" placeholder="1699.95" v-model="product.price">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_start_date" class="form-label">Başlangıç Tarihi</label>
-                    <input type="datetime-local" class="form-control" id="auction_product_start_date">
+                    <input type="datetime-local" class="form-control" id="auction_product_start_date" v-model="product.startDate">
                 </div>
                 <div class="col-md-6">
                     <label for="auction_product_end_date" class="form-label">Bitiş Tarihi</label>
-                    <input type="datetime-local" class="form-control" id="auction_product_end_date">
+                    <input type="datetime-local" class="form-control" id="auction_product_end_date" v-model="product.endDate">
                 </div>
                 <div class="col-md-6">
                     <label for="product_images" class="form-label">Ürün Resimleri</label>
@@ -65,36 +64,96 @@
                 </div>
             </form>
 
-            <button type="submit">Ürünü Ekle</button>
+            <button type="submit" @click="addAuctionProduct">Ürünü Ekle</button>
 
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
     export default {
         data() {
             return {
-            previewImages: [], // Array to store image previews
-            uploadedFiles: []  // Array to store the actual files
+            previewImages: [],
+            product: {
+                name: 'Ürün İsmi',
+                description: 'abc',
+                price: null,
+                sku: '1234',
+                brand: null,
+                model: null,
+                caseColor: null,
+                caseShape: null,
+                bandColor: null,
+                dialColor: null,
+                gender: null,
+                technology: null,
+                isAvailable: true,
+                stockCount: 1,
+                productImages: null,
+                userId: null,
+                startingPrice: null,
+                startDate: null,
+                endDate: null,
+                categoryId: "3deabc52-a8e6-47df-a211-fac748c6c979"
+            }
             };
         },
         methods: {
             handleFileUpload(event) {
-            this.previewImages = []; // Clear existing previews
-            this.uploadedFiles = []; // Clear existing files
+                this.previewImages = []; // Clear existing previews
+                this.product.productImages = [];
 
-            const files = event.target.files;
+                const files = event.target.files;
 
-            Array.from(files).forEach(file => {
-                this.uploadedFiles.push(file); // Store the file
+                Array.from(files).forEach(file => {
+                    let path = "/tez-frontend/images/";
+                    this.product.productImages.push({ path: path + file.name });
 
-                const reader = new FileReader();
-                reader.onload = e => {
-                this.previewImages.push(e.target.result); // Add preview image URL
-                };
-                reader.readAsDataURL(file); // Convert file to Data URL for preview
-            });
+                    const reader = new FileReader();
+                    reader.onload = e => {
+                        this.previewImages.push(e.target.result); // Add preview image URL
+                    };
+                    reader.readAsDataURL(file); // Convert file to Data URL for preview
+                });
+            },
+            logProduct(){
+                console.log(this.product);
+            },
+            formatDateToISO(dateString) {
+                const localDate = new Date(dateString); // Parse the input as local time
+                return new Date(localDate.getTime() - localDate.getTimezoneOffset() * 60000).toISOString();
+            },
+            async addAuctionProduct(){
+                this.product.startingPrice = this.product.price;
+                let token = jwtDecode(this.$store.state.token);
+                this.product.userId = token.Id;
+
+                if (this.product.startDate) {
+                    this.product.startDate = this.formatDateToISO(this.product.startDate);
+                }
+                if (this.product.endDate) {
+                    this.product.endDate = this.formatDateToISO(this.product.endDate);
+                }
+
+                this.logProduct();
+
+                try {
+                    let response = await axios.post('http://18.196.156.3:8080/api/auction/create-auction', this.product, {
+                        headers: {
+                            Authorization: `Bearer ${this.$store.state.token}`
+                        }
+                    });
+
+                    console.log(response.data);
+                    alert("Müzayede ürününüz başarıyla eklendi!");
+                } catch (error) {
+                    console.error(error);
+                    alert("Bir hata oluştu");
+                }
             }
         },
         beforeMount(){
